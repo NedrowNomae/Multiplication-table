818acee123566168c07ada96ddecba80fa5dd367
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-13 12:38:47 -0400

    Updated from Brython Server: 9/13/2018 12:38:47 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -23,10 +23,7 @@ Height of multiplication table: 8
 """
 width = input("Width of multiplication table: ")
 height = input("Height of multiplication table: ")
-row = [x for x in range(1,int(width)+1)]
-notrow = [x for x in range(1,int(height)+1)]
-print(row)
-print(notrow)
-for x in range(int(width)):
-    value = int(row)*int(notrow)
-    print(value)
+for x in range(1,int(height)+1):
+    list = [x*q for q in range(1,int(width)+1)]
+    for x in list:
+        print(str(x) + " ")
