c780f0424b1937d1e3304f41106474434cbc19c7
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-13 12:43:53 -0400

    Updated from Brython Server: 9/13/2018 12:43:52 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -25,4 +25,4 @@ width = input("Width of multiplication table: ")
 height = input("Height of multiplication table: ")
 for x in range(1,int(height)+1):
     list = [x*q for q in range(1,int(width)+1)]
-    print(
+    print(list(0:e:1) for e in range(1,int(width)+1))
